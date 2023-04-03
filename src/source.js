6848a3fcaf1d6b6b5abe23d6758f1ec6b46160bd
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-04-03 23:06:53 +0200

    add array of projects

--- src/source.js
+++ src/source.js
@@ -9,4 +9,74 @@ const skills = [
   'Git',
 ];
 
-export { skills };
+const projects = [
+  {
+    image: '',
+    name: 'Cocktails',
+    description:
+      'In this app you can find your favourite cocktail aссompanied by the description and the ingredients. Natty and tasty design, responsive layout.',
+    stack: [
+      'React',
+      'React Router',
+      'React Context',
+      'Styled Components',
+      'ThCocktalDB API',
+    ],
+    sourceCode: 'https://github.com/Yuliia-Harkusha/cocktails',
+    livePreview: 'https://yuliia-harkusha.github.io/cocktails/',
+  },
+  {
+    image: '',
+    name: 'Weather App',
+    description:
+      'A simple one-page site for finding current weather in different cities simultaneously displaying a picture of the determined city.',
+    stack: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Axios',
+      'OpenWeatherMap API',
+      'Unsplash API',
+    ],
+    sourceCode: 'https://github.com/Yuliia-Harkusha/weather-app',
+    livePreview: 'https://yuliia-harkusha.github.io/weather-app/',
+  },
+  {
+    image: '',
+    name: 'PhoneBook',
+    description:
+      'A small application that includes all the necessary functionality of the phone book.',
+    stack: ['React', 'React Router', 'Redux', 'Styled Components'],
+    sourceCode: 'https://github.com/Yuliia-Harkusha/phonebook-app',
+    livePreview: 'https://yuliia-harkusha.github.io/phonebook-app/',
+  },
+  {
+    image: '',
+    name: 'Petly',
+    description:
+      'Team project where I made  OurFriends/Partners section,  both backend and frontend parts. The idea of the application is to help users to find, sell or buy pets.',
+    stack: [
+      'React',
+      'React Router',
+      'Redux',
+      'Styled Components',
+      'Node.js',
+      'MongoDB',
+      'Mongoose',
+      'Axios',
+    ],
+    sourceCode: 'https://github.com/Yuliia-Harkusha/petly-frontend',
+    livePreview: 'https://petly-frontend.vercel.app/',
+  },
+  {
+    image: '',
+    name: 'WebStudio',
+    description:
+      'My first learning project going from the basics of HTML/CSS to a full-fledged cross-browser and adaptive layout.',
+    stack: ['HTML', 'CSS', 'JavaScript', 'SCSS'],
+    sourceCode: 'https://github.com/Yuliia-Harkusha/Web-studio',
+    livePreview: 'https://yuliia-harkusha.github.io/Web-studio/',
+  },
+];
+
+export { skills, projects };
