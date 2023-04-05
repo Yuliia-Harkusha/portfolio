cf885b2cd0df854fe26634160484596ca44d17b9
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-04-05 12:13:42 +0200

    add react slick slider

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,7 @@
 import { nanoid } from 'nanoid';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { projects } from 'source';
 import { SingleProject } from './SingleProject';
 import './Projects.css';
@@ -6,14 +9,44 @@ import './Projects.css';
 export const Projects = () => {
   if (!projects.length) return null;
 
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section id="projects" className="section">
       <h2 className="section__title">Projects</h2>
 
       <div className="projects">
-        {projects.map(project => (
-          <SingleProject key={nanoid()} project={project} />
-        ))}
+        <Slider {...settings}>
+          {projects.map(project => (
+            <SingleProject key={nanoid()} project={project} />
+          ))}
+        </Slider>
       </div>
     </section>
   );
