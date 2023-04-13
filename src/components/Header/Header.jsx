15fb80e9548936ad0e1626c118733a99c1381f28
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-04-13 21:50:40 +0200

    add sticky position to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import { Navbar } from 'components/Navbar/Navbar';
 import './Header.css';
 
 export const Header = () => {
+  const [headerColor, setHeaderColor] = useState(false);
+
+  const scrollHandler = () => {
+    if (window.scrollY >= 30) {
+      setHeaderColor(true);
+    } else {
+      setHeaderColor(false);
+    }
+  };
+
+  window.addEventListener('scroll', scrollHandler);
+
   return (
-    <header className="header center">
+    <header className={headerColor && 'sticky'}>
       <div className="header-wrapper center">
         <h2>
           <a
